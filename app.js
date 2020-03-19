b5a29ff5c8ab69674e759b572a55ffed00235315
jonathan <jonathanchw@gmail.com>
2020-03-19 13:05:49 -0300

    Listo coordenadas ubicacion

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ let lat;
 
 if (navigator.geolocation){
   navigator.geolocation.getCurrentPosition(position =>{
-    console.log(position)
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+   const proxy = `https://cors-anywhere.herokuapp.com/`;
+    const  api = `${proxy}https://api.darksky.net/forecast/12af3dca9e389c687c75482fedfb4d3d/${lat},${long}`; 
+
+  fetch(api)
+  .then(Response =>{
+return Response.json();
+
+    })
+    .then(data =>{
+      console.log(data);
+    }); 
 });
 }
-
 
 });
